3f0f3c3d09c5048706612ac93df54feea554aeb5
chiendarkduck <nguyenhuuchien17112k2@gmail.com>
2023-07-19 08:31:59 +0700

    jsbasic

---
+++ main.js
@@ -0,0 +1,24 @@
+// alert('chien dep trai')
+console.log('chien dep trai');
+let fullName=`Nguyen Huu Chien`;
+let age=21;
+
+
+//setinterval sau bn giay chay lap lai code 1 lan
+// let i=0;
+// let timer = setInterval(() => {
+//     alert(age);
+//     i++;
+//     if(i===5){
+//         clearInterval(timer);
+//     }
+// }, 1000);
+
+
+//setinterval sau bn giay se chay code
+// let timer1=setTimeout(() => {
+//     alert(fullName);
+// },5000)
+alert('chien rat rat dep trai');
+confirm('xac nhan ban du tuoi');
+prompt("xac nha du tuoi")
