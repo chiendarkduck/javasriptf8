7e6284f702c29678b60beff5017e4602d6601696
chiendarkduck <nguyenhuuchien17112k2@gmail.com>
2023-07-19 09:05:05 +0700

    bai 4 tien to va hau to

--- main.js
+++ main.js
@@ -42,25 +42,35 @@ let age=21;
 //     console.log('x va y lon hon khong')
 // }
 
-//bai 3---toan tu so hoc
-//cong +
-let a=9,b=5,c=3;
-console.log('cong a b c=',a+b+c)
-//tru -
-console.log('c tru b tru a =',c-b-a)
-//nhan *
-console.log(`a nhan b =`,a*b)
-//nhan luy thua **
-console.log(`luy thua c=`,b**c)
-//chia /
-console.log(`a chia b=`,a/b)
-//chia lay phan du %
-console.log(`a chia b lay phan du=`,a%b)
-//tang 1 gia tri so ++
-a++;
-console.log(`a tang len 1 lan=`,a)
-//giam 1 gia tri so --
-a--;
-console.log(`a  giam 1 gia tri so`,a)
+// //bai 3---toan tu so hoc
+// //cong +
+// let a=9,b=5,c=3;
+// console.log('cong a b c=',a+b+c)
+// //tru -
+// console.log('c tru b tru a =',c-b-a)
+// //nhan *
+// console.log(`a nhan b =`,a*b)
+// //nhan luy thua **
+// console.log(`luy thua c=`,b**c)
+// //chia /
+// console.log(`a chia b=`,a/b)
+// //chia lay phan du %
+// console.log(`a chia b lay phan du=`,a%b)
+// //tang 1 gia tri so ++
+// a++;
+// console.log(`a tang len 1 lan=`,a)
+// //giam 1 gia tri so --
+// a--;
+// console.log(`a  giam 1 gia tri so`,a)
 
-
+//bai 4 ++ --
+//prefix & postfix--- tien to ,hau to
+let a=6;
+a++;//hau to gan gia tri cho a va tang len 1 dv
+console.log(a);
+a--;//gan gia tri a va giam 1 dv
+console.log(a);
+++a;//tang 1 dv sau do gan cho a
+console.log(a);
+--a;//giam 1 dv sau do gan cho a
+console.log(a);
